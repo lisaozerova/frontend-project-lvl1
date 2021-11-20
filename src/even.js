d8143ef9782fffc91c8d237e9543cd9a07eca6a6
Elizaveta Ozerova <e.ozerova@adsterra.com>
2021-11-20 20:06:13 +0300

    refactor brain even game

--- src/even.js
+++ src/even.js
@@ -14,23 +14,23 @@ const GamePhrase = {
 
 const ROUND_COUNT = 3;
 
-const gameNumbers = [15, 6, 7];
-
 const getCorrectAnswerPhrase = (value) => (value ? GamePhrase.YES : GamePhrase.NO);
 
 const isNumberParity = (number) => number % 2 === 0;
+
+const getRandomIntInclusive = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const playEven = (name) => {
   console.log(GamePhrase.RULES);
   let round = 0;
 
-  for (round; round < gameNumbers.length; round += 1) {
-    const number = gameNumbers[round];
-    const userAnswer = readlineSync.question(`${GamePhrase.QUESTION} ${number}\r\n${GamePhrase.ANSWER}`)
+  for (round; round < ROUND_COUNT; round += 1) {
+    const num = getRandomIntInclusive(1, 100);
+    const userAnswer = readlineSync.question(`${GamePhrase.QUESTION} ${num}\r\n${GamePhrase.ANSWER}`)
       .trim()
       .toLowerCase();
 
-    const correctAnswer = getCorrectAnswerPhrase(isNumberParity(number));
+    const correctAnswer = getCorrectAnswerPhrase(isNumberParity(num));
     const isUserAnswerCorrect = correctAnswer === userAnswer;
 
     if (isUserAnswerCorrect) {
